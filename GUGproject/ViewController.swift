c62f88b0963fbbb71aba04352c66e1c4addd0f04
AhmariOnaim313 <blank@gmail.com>
2020-07-14 07:57:46 -0700

    project

---
+++ GUGproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GUGproject
+//
+//  Created by Apple on 7/13/20.
+//  Copyright © 2020 MK, Nasrin, Kiley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
